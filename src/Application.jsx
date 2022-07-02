d9a313908d57596412c6678269eec8f0c13ae238
Samirka7 <90614000+Samirka7@users.noreply.github.com>
2022-07-02 14:53:30 +0700

    .

---
+++ src/Application.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { block } from "bem-cn";
+
+import ControlPanel from "./components/ControlPanel/ControlPanel";
+import OrdersList from "./components/OrdersList/OrdersList";
+import Api from "./api/api";
+import "./Application.css";
+
+const b = block("application");
+const privateKey = "8e49ff607b1f46e1a5e8f6ad5d312a80";
+const publicKey = "38cd79b5f2b2486d86f562e3c43034f8";
+const api = new Api(privateKey, publicKey);
+
+class Application extends React.Component {
+  state = {
+    ordersList: [],
+    numberOfOrders: 10,
+    status: "NotProcessed",
+  };
+
+  componentDidMount() {
+    const { numberOfOrders, status } = this.state;
+    api
+      .getSomeAmountOfOrders(numberOfOrders, status)
+      .then((orders) => this.setState({ ordersList: orders }));
+  }
+
+  render() {
+    const { ordersList, numberOfOrders, status } = this.state;
+
+    return (
+      <div className={b()}>
+        <header className={b("header")}>
+          Приложение для вывода данных о заказах Pixlpark
+        </header>
+        <div className={b("panel")}>
+          <ControlPanel
+            onLoadButtonClickHandler={this.handleLoadButtonClick}
+            onInputChangeHandler={this.handleInputChange}
+            onSelectChangeHandler={this.handleSelectChange}
+            status={status}
+            numberOfOrders={numberOfOrders}
+          />
+        </div>
+        <div className={b("content")}>
+          <div className={b("orders-list")}>
+            <OrdersList data={ordersList} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  handleLoadButtonClick = () => {
+    const { numberOfOrders, status } = this.state;
+    api
+      .getSomeAmountOfOrders(numberOfOrders, status)
+      .then((orders) => this.setState({ ordersList: orders }));
+  };
+
+  handleInputChange = (event) => {
+    this.setState({ numberOfOrders: event.target.value });
+  };
+
+  handleSelectChange = (event) => {
+    this.setState({ status: event.target.value });
+  };
+}
+
+export default Application;
---
+++ src/components/ControlPanel/ControlPanel.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { block } from "bem-cn";
+
+import "./ControlPanel.css";
+
+const b = block("control-panel");
+
+class ControlPanel extends React.Component {
+  render() {
+    const {
+      onLoadButtonClickHandler,
+      onInputChangeHandler,
+      onSelectChangeHandler,
+      status,
+      numberOfOrders,
+    } = this.props;
+
+    return (
+      <div className={b()}>
+        <div className={b("header")}>{"Фильтры"}</div>
+        <div className={b("filter")}>
+          {"Статус: "}
+          <select value={status} onChange={onSelectChangeHandler}>
+            <option value="NotProcessed">Не обработан</option>
+            <option value="Cancelled">Отменен</option>
+            <option value="Printing">В печати</option>
+          </select>
+        </div>
+        <div className={b("order-number")}>
+          {"Количество ордеров к загрузке: "}
+          <input
+            type="number"
+            value={numberOfOrders}
+            onChange={onInputChangeHandler}
+          />
+        </div>
+        <div className={b("button-wrapper")}>
+          <button
+            className={b("load-button")}
+            onClick={onLoadButtonClickHandler}
+          >
+            {"Вывод заказов с сервера"}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ControlPanel;
